12a01a01ec9c7466a0927c541a1ed2d0e640c065
Johnomaji <Johnomaji99@gmail.com>
2025-04-18 00:40:40 +0100

    i don't know but i trust

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.querySelector('.toggle-btn');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
